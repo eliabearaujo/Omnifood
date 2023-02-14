4aa921669cf040cc91ddda5d4098dba0e879b8aa
eliabearaujo <eliabe_trajano@hotmail.com>
2023-02-13 23:19:45 -0300

    Navegação mobile finalizada

---
+++ script.js
@@ -0,0 +1,6 @@
+const btnNav = document.querySelector('.btn-mobile-nav');
+const header = document.querySelector('.header');
+
+btnNav.addEventListener('click', () => {
+  header.classList.toggle('nav-open');
+});
