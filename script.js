3118ab7243e7af0c95263481766b9a1a5be6898b
eliabearaujo <eliabe_trajano@hotmail.com>
2023-02-14 21:48:33 -0300

    Sticky Nav finalizado

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
 const btnNav = document.querySelector('.btn-mobile-nav');
 const header = document.querySelector('.header');
 
-btnNav.addEventListener('click', () => {
+btnNav.addEventListener('click', (e) => {
   header.classList.toggle('nav-open');
 });
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+    if (href === '#') {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    if (link.classList.contains('main-nav-link')) {
+      header.classList.toggle('nav-open');
+    }
+  });
+});
+
+const sectionHero = document.querySelector('.section-hero');
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add('sticky');
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove('sticky');
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px',
+  }
+);
+obs.observe(sectionHero);
